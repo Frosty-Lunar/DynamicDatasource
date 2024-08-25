baa8fe8101ccae1061afea0070e6fdaeb897dc83
Elara <201628142@qq.com>
2024-08-25 10:44:13 +0800

    初始化项目

--- src/main/java/com/example/dynamicdatasource/DynamicDatasourceApplication.java
+++ src/main/java/com/example/dynamicdatasource/DynamicDatasourceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DynamicDatasourceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DynamicDatasourceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDatasourceApplication.class, args);
+    }
 
 }
